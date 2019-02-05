0b825e0a69d792375cb0ee0d476b126f2321f96e
Gordon Crawford <GCrawford1@statestreet.com>
2019-02-05 14:52:27 +0000

    Adding initial dto files.

---
+++ gowaf_test.go
@@ -0,0 +1,17 @@
+package gowaf
+
+import (
+	"fmt"
+	"testing"
+)
+
+// this is a comment
+
+func TestGOWAF(t *testing.T) {
+	fmt.Println("GOWAF")
+}
+
+func TestDTO(t *testing.T) {
+	d := dto{}
+	fmt.Println("pass")
+}
