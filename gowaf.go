990b60f777c528d1be752c8abcb436cfc2e76e85
Gordon Crawford <GCrawford1@statestreet.com>
2019-02-05 16:49:43 +0000

    Made the db connection exposed from the dto package.

--- gowaf.go
+++ gowaf.go
@@ -8,6 +8,26 @@ import (
 
 func main() {
 	fmt.Println("gowaf")
-	d := dto.Dto{}
-	fmt.Println(d)
+	db, error := dto.InitDTO("root@/loa")
+	if error == nil {
+		rows, err := db.Query("SELECT * FROM player")
+		if err == nil {
+			for rows.Next() {
+				var id int
+				var name string
+				var familyname string
+				var craft int
+				err = rows.Scan(&id, &name, &familyname, &craft)
+
+				fmt.Println(id)
+				fmt.Println(name)
+				fmt.Println(familyname)
+				fmt.Println(craft)
+
+			}
+		}
+		db.Close()
+	}
 }
+
+// query
--- include/dto/dto.go
+++ include/dto/dto.go
@@ -1,6 +1,30 @@
 package dto
 
-type Dto struct {
+import (
+	"database/sql"
+
+	// importing the SQL driver
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// DTO structure
+type DTO struct {
 	id     int
 	status string
+}
+
+func InitDTO(dataSourceName string) (*sql.DB, error) {
+	if len(dataSourceName) == 0 {
+		dataSourceName = "root@/loa"
+	}
+	db, err := sql.Open("mysql", dataSourceName)
+	checkErr(err)
+
+	return db, err
+
+}
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
